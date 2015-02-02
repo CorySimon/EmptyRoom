576484c007f98040038dbc9e7c50040dca81c689
Cory Simon <corymsimon@barracuda.lbl.gov>
2015-02-02 14:31:06 -0800

    forcefield working

---
+++ Forcefield.h
@@ -0,0 +1,25 @@
+/*
+ * Forcefield.h
+ *   loads information from force field file
+ *   Stores Lennard-Jones potentials of form 4 eps * ( (sig/r)^12 - (sig/r)^6 )
+ *  Created on: Feb 2, 2015
+ *      Author: corymsimon
+ */
+using namespace std;
+#include<vector>
+#include<string>
+
+#ifndef FORCEFIELD_H_
+#define FORCEFIELD_H_
+
+class Forcefield {
+public:
+	Forcefield(string forcefield, bool verbose=true);
+
+    int nointeractions; // number of interactions defined
+    vector<double> epsilon; // LJ parameter units: K
+    vector<double> sigma; // LJ parameter units: A
+    vector<string> identity; // labels of atoms that correspond to framework and adsorbate objects
+};
+
+#endif /* FORCEFIELD_H_ */
