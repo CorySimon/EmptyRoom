2985c2b1e393065bbf0a8fd2dfea1870cdc21c26
Cory <cory@ubuntu.ubuntu-domain>
2015-07-10 16:35:35 -0700

    print uc

--- print_uc.jl
+++ print_uc.jl
@@ -1,9 +1,23 @@
 include("julia_codes/framework.jl")
 
-structures = readdlm("structurelist.txt")
-@printf("Writing unit cell replication files with cutoff radius %f A...\n", float64(ARGS[1]))
+if (length(ARGS) == 1)
+    """
+    Get UC reps for all structures in structurelist.txt
+    """
+    structures = readdlm("structurelist.txt")
+    @printf("Writing unit cell replication files with cutoff radius %f A...\n", float64(ARGS[1]))
 
-for k = 1:length(structures)
-    write_uc_replication_file(structures[k], float64(ARGS[1]), "once")
-    write_uc_replication_file(structures[k], float64(ARGS[1]), "twice")
+    for k = 1:length(structures)
+        write_uc_replication_file(structures[k], float64(ARGS[1]), "once")
+        write_uc_replication_file(structures[k], float64(ARGS[1]), "twice")
+    end
+end
+
+if (length(ARGS) == 2)
+    """
+    first arg is cutoff, second arg is structure
+    """
+    @printf("Writing unit cell replication files for structure %s with cutoff radius %f A...\n", ARGS[2], float64(ARGS[1]))
+    write_uc_replication_file(ARGS[2], float64(ARGS[1]), "once")
+    write_uc_replication_file(ARGS[2], float64(ARGS[1]), "twice")
 end
